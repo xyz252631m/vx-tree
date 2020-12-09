a77a6aa75b78d66fb6393631197046436b586bd2
xyz252631m <1325089891@qq.com>
2020-12-09 22:05:15 +0800

    ]

---
+++ src/components/tree/Tree.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+  	<li v-for="m in list"></li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  name: 'Tree',
+  data(){
+      return {
+          list:[]
+      }
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+  
+</style>
