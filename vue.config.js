86f76c1457d2f0ed101211b56603d1acddffa8b2
wangwei <wangwei@163.com>
2021-01-21 18:13:54 +0800

    Default Changelist

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
     devServer: {
         port:8088
     },
+    // publicPath: process.env.NODE_ENV === 'production' ? '/examples/' : '/',
     // publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
     // entry: process.env.NODE_ENV === 'dev'? path.resolve(__dirname, './src/main.js'): path.resolve(__dirname, './index.js'),
     // output: {
@@ -15,14 +16,19 @@ module.exports = {
     // externals: {
     //     vue: 'vue'
     // }
-    css: {
-        loaderOptions: {
-            less: {
-                lessOptions: {
-                    javascriptEnabled: true
-                }
-            }
-
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'examples/main.js',
+            // 模板来源
+            template: 'public/index.html',
+            title: '示例',
+        },
+    },
+    configureWebpack: {
+        output: {
+            libraryExport: 'default'
         }
     },
+    productionSourceMap:false
 }
